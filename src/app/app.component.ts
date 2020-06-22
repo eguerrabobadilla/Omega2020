635d2ee9016a43f58ce0cdc03afa44b52895991a
ingjosecruzp <ing.josecruzp@gmail.com>
2020-06-22 10:46:50 -0600

    varios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class AppComponent {
   }
 
   initializeApp() {
+    console.log("initializeApp");
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
