d6570d22330b23af5e084e2ab47cdf1c87e2e5ac
Eduardo <Eduardo@DESKTOP-QPUNTCT>
2020-10-22 00:23:19 -0500

    eduardo

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, Renderer2, ViewChild, Output, EventEmitter, ViewChildren, QueryList, Input } from '@angular/core';
 import { IonItem, PickerController, LoadingController } from '@ionic/angular';
 import { EstadisticasService } from '../../api/estadisticas.service';
+import { UsuariosService } from 'src/app/api/usuarios.service';
 
 @Component({
   selector: 'app-report',
@@ -13,6 +14,16 @@ export class ReportComponent implements OnInit {
   mesActual : string;
   meses: string[];
   loading: any;
+  public user = {
+    NombreCompleto : '',
+    Grado  : '',
+    Grupo : '',
+    Escolaridad: '',
+    GradoIngles: '',
+    GrupoIngles: '',
+    Usuario: '',
+    Tipo: ''
+  };
   @ViewChildren(IonItem) ArrayItems: QueryList<IonItem>;
   @ViewChildren('itemsref', {read: ElementRef}) itemsref: QueryList<ElementRef>;
   @ViewChildren('itemsrefBoton', {read: ElementRef}) itemsrefBoton: QueryList<ElementRef>;
@@ -30,11 +41,11 @@ export class ReportComponent implements OnInit {
   @ViewChild('botonKinder3', {read: ElementRef, static: false}) botonKinder3: ElementRef;
 
   @Output() updateAutoHeightSlider = new EventEmitter();
-  items:any = [];
+  items: any = [];
 
 
-  constructor(private renderer: Renderer2,private apiEstadisticas: EstadisticasService, private pickerController: PickerController,
-              private loadingController: LoadingController) { }
+  constructor(private renderer: Renderer2, private apiEstadisticas: EstadisticasService, private pickerController: PickerController,
+              private loadingController: LoadingController,private apiUsuarios: UsuariosService) { }
 
   ngOnInit() {
 
@@ -44,20 +55,33 @@ export class ReportComponent implements OnInit {
     const mesesReal = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo',
                   'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     const d = new Date();
-    this.mesActual= mesesReal[d.getMonth()];
+    this.mesActual = mesesReal[d.getMonth()];
 
     this.apiEstadisticas.getEstadisticasAlumno('10').subscribe(data => {
     this.LstEstadisticas = data;
     console.log(this.LstEstadisticas);
   });
 
+    const UsuarioId =this.getKeyToken('id');
+    this.apiUsuarios.getUsuario(UsuarioId).subscribe(data => {
+     this.user.NombreCompleto = data["NombreCompleto"];
+     this.user.Grado = data["Grado"];
+     this.user.Grupo = data["Grupo"];
+     this.user.Escolaridad = data["Escolaridad"];
+     this.user.GradoIngles = data["GradoIngles"];
+     this.user.GrupoIngles = data["GrupoIngles"];
+     this.user.Usuario = data["Usuario"];
+     this.user.Tipo = data["Tipo"];
+  });
+
+
   }
 
-  abrirMateria(index,ele){
+  abrirMateria(index, ele){
 
     this.itemsref.forEach(itemsref => {
 
-      if(itemsref.nativeElement.id == index){
+      if (itemsref.nativeElement.id == index){
         this.aplicarEstilos(itemsref.nativeElement);
       }
       });
@@ -103,17 +127,17 @@ export class ReportComponent implements OnInit {
     const statusPanel = item.getAttribute("status");
 
     console.log(item);
-    if(statusPanel=="close") {
+    if (statusPanel == "close") {
       this.renderer.removeStyle(item, 'max-height');
    //   this.renderer.addClass(item.botonKinder.nativeElement, 'active');
-      item.setAttribute('status','open');
+      item.setAttribute('status', 'open');
       this.renderer.removeStyle(item, 'display');
     }
     else {
-      this.renderer.setStyle(item, 'max-height','0');
+      this.renderer.setStyle(item, 'max-height', '0');
   //    this.renderer.removeClass(item.botonKinder.nativeElement, 'active');
-      item.setAttribute('status','close');
-      this.renderer.setStyle(item, 'display','none');
+      item.setAttribute('status', 'close');
+      this.renderer.setStyle(item, 'display', 'none');
     }
   }
 
@@ -122,20 +146,20 @@ export class ReportComponent implements OnInit {
     const options = [];
 
     this.meses.forEach((x, index) => {
-      let month = index+1;
+      let month = index + 1;
 
-      if(month==1) month = 8;
-      else if(month==2) month = 9;
-      else if(month==3) month = 10;
-      else if(month==4) month = 11;
-      else if(month==5) month = 12;
-      else if(month==6) month = 1;
-      else if(month==7) month = 2;
-      else if(month==8) month = 3;
-      else if(month==9) month = 4;
-      else if(month==10) month = 5;
-      else if(month==11) month = 6;
-      else if(month==12) month = 7;
+      if (month == 1) month = 8;
+      else if (month == 2) month = 9;
+      else if (month == 3) month = 10;
+      else if (month == 4) month = 11;
+      else if (month == 5) month = 12;
+      else if (month == 6) month = 1;
+      else if (month == 7) month = 2;
+      else if (month == 8) month = 3;
+      else if (month == 9) month = 4;
+      else if (month == 10) month = 5;
+      else if (month == 11) month = 6;
+      else if (month == 12) month = 7;
 
       options.push({text: x , value: month});
     });
@@ -154,17 +178,17 @@ export class ReportComponent implements OnInit {
 
    console.log(month);
 
-    if(month==1) month= 5;
-    else if(month==2) month= 6;
-    else if(month==3) month= 7;
-    else if(month==4) month= 8;
-    else if(month==5) month= 9;
-    else if(month==6) month= 10;
-    else if(month==7) month= 11;
-    else if(month==8) month= 0;
-    else if(month==9) month= 1;
-    else if(month==10) month= 2;
-    else if(month==11) month= 3;
+   if (month == 1) month = 5;
+    else if (month == 2) month = 6;
+    else if (month == 3) month = 7;
+    else if (month == 4) month = 8;
+    else if (month == 5) month = 9;
+    else if (month == 6) month = 10;
+    else if (month == 7) month = 11;
+    else if (month == 8) month = 0;
+    else if (month == 9) month = 1;
+    else if (month == 10) month = 2;
+    else if (month == 11) month = 3;
 
 
    console.log(month);
@@ -179,4 +203,20 @@ export class ReportComponent implements OnInit {
 
     await this.loading.present();
   }
+
+  getKeyToken(key: string): string {
+
+    const jwt = localStorage.getItem('USER_INFO');
+
+    const jwtData = jwt.split('.')[1];
+    // let decodedJwtJsonData = window.atob(jwtData);
+    const decodedJwtJsonData = decodeURIComponent(escape(window.atob(jwtData)));
+    const decodedJwtData = JSON.parse(decodedJwtJsonData);
+
+    const value = decodedJwtData[key];
+
+    return value;
+  }
+
+
 }
