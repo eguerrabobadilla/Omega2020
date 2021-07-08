5d2230c3f9c05e8df3a338be9da5a2f06a863500
ingjosecruzp <ing.josecruzp@gmail.com>
2021-07-07 22:28:21 -0600

    Version 65 config

--- src/app/api/apiBase.ts
+++ src/app/api/apiBase.ts
@@ -7,13 +7,13 @@ import { Injectable } from '@angular/core';
 export class apiBase  {
     //url  : string = "https://172.16.12.164:5001";
    //  url  : string = "https://172.16.12.10:5001";
-    url: string = "https://localhost:5001";
+    //url: string = "https://localhost:5001";
     //   url: string = "https://192.168.0.3:5001";
    //url: string = "https://192.168.137.1:5001";
     //url: string = "http://35.193.103.213";
     //url: string = "http://192.168.137.1:5000";
     //  url: string = "http://35.193.103.213";
       //url: string = 'https://192.168.0.2:5001';
-  //  url: string = 'https://www.alfalbs.app/ApiOmega';
+    url: string = 'https://www.alfalbs.app/ApiOmega';
     Ws: string;
 }
